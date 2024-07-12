393c98e8cb4e7b8768ff872074c0af598b01d5f5
Danny Vargas <danny@av-controls.com>
2024-07-12 10:30:39 -0400

    new build

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,21 @@ export default defineConfig({
   plugins: [
     vue()
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        // Customize the output paths for assets, CSS, and JS
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpg|png|svg|webp)$/.test(name ?? '')) {
+            return 'assets/[name]-[hash][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+        cssFileNames: 'css/[name]-[hash].css',
+      },
+    },
+  },
 });
 
